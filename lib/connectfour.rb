e038cdb30593afcd0f46a42205e990e915f4a77c
Nikolay Kolev <nikolay.kolev@icb.bg>
2012-12-23 13:24:59 +0200

    The structure of the project's library is created

---
+++ lib/connectfour.rb
@@ -0,0 +1,4 @@
+require 'connectfour/matrix_iterator'
+require 'connectfour/board'
+require 'connectfour/score_calculator'
+require 'connectfour/game_engine'
---
+++ spec/connectfour/score_calculator_spec
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module ConnectFour
+  describe ScoreCalculator do
+
+  end
+end
