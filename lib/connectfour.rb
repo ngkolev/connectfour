73bf4e8f12673f367ab01b6b8b990387036f55e6
Nikolay Kolev <nikolay.kolev@icb.bg>
2012-12-25 13:43:35 +0200

    Matrix visitors implementation and (connectfour -> connect_four) rename

--- lib/connectfour.rb
+++
@@ -1,9 +0,0 @@
-require 'connectfour/matrix_visitor/first_diagonal_matrix_visitor'
-require 'connectfour/matrix_visitor/horizontal_matrix_visitor'
-require 'connectfour/matrix_visitor/second_diagonal_matrix_visitor'
-require 'connectfour/matrix_visitor/vertical_matrix_visitor'
-require 'connectfour/matrix_visitor/visitor_collection'
-
-require 'connectfour/board'
-require 'connectfour/score_calculator'
-require 'connectfour/game_engine'
